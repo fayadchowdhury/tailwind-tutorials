c06d419aef66b86d4e4c7d01f03b9f06956b9a3a
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-05-06 00:47:04 -0700

    Demonstrate tailwind's JIT compiler which allows creating and computing exactly the CSS required

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-2">
-      <h1 className="text-xl text-green-500 text-center font-mono">
+      <h1 className="text-[35px] text-[#9e7171] text-center font-mono">
         Vite + React + TypeScript + TailwindCSS
       </h1>
     </div>
